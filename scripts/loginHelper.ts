0dd61edf25558f7c64ffd57c53c8d4ff510117c0
Yannick Borschneck <yannick.borschneck@gmail.com>
2025-07-03 18:03:08 +0200

    Catch empty device_token

--- scripts/loginHelper.ts
+++ scripts/loginHelper.ts
@@ -1,11 +1,10 @@
-import { encode as base64Encode } from "base-64";
+import {encode as base64Encode} from "base-64";
 import * as SecureStore from 'expo-secure-store';
-import { getCSRFToken } from "./helper/CSRFTokenHelper";
+import {getCSRFToken} from "./helper/CSRFTokenHelper";
 
 //Get CSRF Token from secure storage
 async function fetchCSRFfromSecureStorage() {
-    var result = await SecureStore.getItemAsync("CSRF_Token");
-    return result;
+    return await SecureStore.getItemAsync("CSRF_Token");
 }
 
 //TODO: Has to return the error when it gets one.
@@ -65,7 +64,11 @@ export default async function performLoginAttempt(usr:string, pw:string, serverA
     await SecureStore.setItemAsync("user_pw", pw);
     await SecureStore.setItemAsync("server_address", serverAddr);
     await SecureStore.setItemAsync("user_staffId", result.id);
-    await SecureStore.setItemAsync("user_deviceToken", result.deviceToken);
 
+    if (result?.deviceToken) {
+        await SecureStore.setItemAsync("user_deviceToken", result.deviceToken);
+    } else {
+        console.warn("deviceToken ist null und wird nicht gespeichert.");
+    }
     return true;
 }
